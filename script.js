bbec962296218f0a6fe48d7aba11dc429d0a1fff
Francine Altman <francinekline@gmail.com>
2026-02-20 21:54:46 -0500

    refactor: remove modals

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 (() => {
   window.addEventListener("DOMContentLoaded", (event) => {
-    const modals = document.querySelectorAll(".modal");
-
-    function displayModal() {
-      let span = this.querySelector('.modal-text')
-      let img = this.querySelector('img')
-      span === null ? span = '' : span = `${span.innerText}`;
-      img === null ? img = '' : img = `${img.src}`;
-      Swal.fire({
-        title: span,
-        imageUrl: img,
-      });
-    }
-
-    for (modal of modals) {
-      modal.addEventListener("click", displayModal);
-    }
-
     document.getElementById("year").innerHTML = new Date().getFullYear();
   });
 })();
